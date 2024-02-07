5519dc54fdea916556deba2c2e3b140d43e5967e
lynrics30 <labutskupskups@gmail.com>
2024-02-07 09:00:09 +0800

    updated

--- script.js
+++ script.js
@@ -48,24 +48,17 @@ function updateStars() {
 }
 
 const button = document.getElementById("valentinesButton");
+const audio = document.getElementById("valentinesAudio");
 
 button.addEventListener("click", () => {
-  if (button.textContent === "Click Me! ❤") {
+  if (button.textContent === "Click mo to!") {
     button.textContent = "loading...";
-    fetch('send_mail.php')
-      .then(response => {
-        if (response.ok) {
-          button.textContent = "Check Your Email 🙃";
-        } else {
-          console.error('Failed to send email');
-          button.textContent = "Error 😞";
-        }
-      })
-      .catch(error => {
-        // Handle network errors or other issues
-        console.error('Error:', error);
-        button.textContent = "Error 😞";
-      });
+    // Play the audio
+    audio.play();
+    button.textContent = "Enjoy Bwisit!";
+    setTimeout(() => {
+        button.remove();
+      }, 3000);
   }
 });
 
@@ -89,14 +82,25 @@ function drawText() {
     context.shadowOffsetY = 0;
 
     if(frameNumber < 250){
+        button.style.display = "block";
+
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+
+        if (window.innerWidth < 600) {           //shortens long sentence for mobile screens
+            drawTextWithLineBreaks(["This is just a simple gift kaya" , "pag tyagaan mo nalang HAHAHAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("This is just a simple gift kaya pag tyagaan mo nalang HAHAHAHAHA", canvas.width/2, canvas.height/2);
+        }
         opacity = opacity + 0.01;
     }
     //fades out the text by decreasing the opacity
     if(frameNumber >= 250 && frameNumber < 500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("everyday day I cannot believe how lucky I am", canvas.width/2, canvas.height/2);
+        if (window.innerWidth < 600) {           //shortens long sentence for mobile screens
+            drawTextWithLineBreaks(["This is just a simple gift kaya" , "pag tyagaan mo nalang HAHAHAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("This is just a simple gift kaya pag tyagaan mo nalang HAHAHAHAHA", canvas.width/2, canvas.height/2);
+        }
         opacity = opacity - 0.01;
     }
 
@@ -108,9 +112,9 @@ function drawText() {
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
 
         if (window.innerWidth < 600) {           //shortens long sentence for mobile screens
-            drawTextWithLineBreaks(["amongst trillions and trillions of stars,", "over billions of years"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["3 days ko din ginawa to, maikli lang naman", "Syempre gagamitin ko skill ko HAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("amongst trillions and trillions of stars, over billions of years", canvas.width/2, canvas.height/2);
+            context.fillText("3 days ko din ginawa to, maikli lang naman Syempre gagamitin ko skill ko HAHAHA", canvas.width/2, canvas.height/2);
         }
 
         opacity = opacity + 0.01;
@@ -119,9 +123,9 @@ function drawText() {
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
         
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["amongst trillions and trillions of stars,", "over billions of years"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["3 days ko din ginawa to, maikli lang naman", "Syempre gagamitin ko skill ko HAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("amongst trillions and trillions of stars, over billions of years", canvas.width/2, canvas.height/2);
+            context.fillText("3 days ko din ginawa to, maikli lang naman Syempre gagamitin ko skill ko HAHAHA", canvas.width/2, canvas.height/2);
         }
 
         opacity = opacity - 0.01;
@@ -132,12 +136,24 @@ function drawText() {
     }
     if(frameNumber > 1000 && frameNumber < 1250){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("to be alive, and to get to spend this life with you", canvas.width/2, canvas.height/2);
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["Gusto ko lang sabihin na Thank you Mommy,", "Di ako nagsisisi na ikaw ang naging bebe ko"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("Gusto ko lang sabihin na Thank you Mommy, Di ako nagsisisi na ikaw ang naging bebe ko", canvas.width/2, canvas.height/2);
+        }
+
         opacity = opacity + 0.01;
     }
     if(frameNumber >= 1250 && frameNumber < 1500){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("to be alive, and to get to spend this life with you", canvas.width/2, canvas.height/2);
+
+        if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["Gusto ko lang sabihin na Thank you Mommy,", "Di ako nagsisisi na ikaw ang naging bebe ko"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("Gusto ko lang sabihin na Thank you Mommy, Di ako nagsisisi na ikaw ang naging bebe ko", canvas.width/2, canvas.height/2);
+        }
+
         opacity = opacity - 0.01;
     }
 
@@ -146,12 +162,24 @@ function drawText() {
     }
     if(frameNumber > 1500 && frameNumber < 1750){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("is so incredibly, unfathomably unlikely", canvas.width/2, canvas.height/2);
+
+         if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["Ang unexpected na gagaan agad yung loob ko sayo,", "Tamang bardagulan lang noon eh HAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("Ang unexpected na gagaan agad yung loob ko sayo, Tamang bardagulan lang noon eh HAHAHA", canvas.width/2, canvas.height/2);
+        }
+
         opacity = opacity + 0.01;
     }
     if(frameNumber >= 1750 && frameNumber < 2000){
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
-        context.fillText("is so incredibly, unfathomably unlikely", canvas.width/2, canvas.height/2);
+
+         if (window.innerWidth < 600) {
+            drawTextWithLineBreaks(["Ang unexpected na gagaan agad yung loob ko sayo,", "Tamang bardagulan lang noon eh HAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+        } else {
+            context.fillText("Ang unexpected na gagaan agad yung loob ko sayo, Tamang bardagulan lang noon eh HAHAHA", canvas.width/2, canvas.height/2);
+        }
+
         opacity = opacity - 0.01;
     }
 
@@ -162,9 +190,9 @@ function drawText() {
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
 
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["and yet here I am to get the impossible", "chance to get to know you"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["Pero dahil don mas nakilala kita", "Mas nainditihan ko kung anong ugali meron ka"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("and yet here I am to get the impossible chance to get to know you", canvas.width/2, canvas.height/2);
+            context.fillText("Pero dahil don mas nakilala kita Mas nainditihan ko kung anong ugali meron ka", canvas.width/2, canvas.height/2);
         }
 
         opacity = opacity + 0.01;
@@ -173,9 +201,9 @@ function drawText() {
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
 
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["and yet here I am to get the impossible", "chance to get to know you"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["Pero dahil don mas nakilala kita,", "Mas nainditihan ko kung anong ugali meron ka"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("and yet here I am to get the impossible chance to get to know you", canvas.width/2, canvas.height/2);
+            context.fillText("Pero dahil don mas nakilala kita, Mas nainditihan ko kung anong ugali meron ka", canvas.width/2, canvas.height/2);
         }
         
         opacity = opacity - 0.01;
@@ -188,9 +216,9 @@ function drawText() {
         context.fillStyle = `rgba(45, 45, 255, ${opacity})`;
 
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["I love you so much {name}, more than", "all the time and space in the universe can contain"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
+            drawTextWithLineBreaks(["Kaya thru ups and down just call daddy,", "kahit sobra yung toyo mo sa katawan HAHAHA"], canvas.width / 2, canvas.height / 2, fontSize, lineHeight);
         } else {
-            context.fillText("I love you so much {name}, more than all the time and space in the universe can contain", canvas.width/2, canvas.height/2);
+            context.fillText("Kaya thru ups and down just call daddy, kahit sobra yung toyo mo sa katawan HAHAHA", canvas.width/2, canvas.height/2);
         }
 
         opacity = opacity + 0.01;
@@ -201,9 +229,9 @@ function drawText() {
 
 
         if (window.innerWidth < 600) {
-            drawTextWithLineBreaks(["and I can't wait to spend all the time in", "the world to share that love with you!"], canvas.width / 2, (canvas.height/2 + 60), fontSize, lineHeight);
+            drawTextWithLineBreaks(["Andito lang ako palagi naka support sayo", "Iloveyouuu so much Bugsbunny ❤"], canvas.width / 2, (canvas.height/2 + 60), fontSize, lineHeight);
         } else {
-            context.fillText("and I can't wait to spend all the time in the world to share that love with you!", canvas.width/2, (canvas.height/2 + 50));
+            context.fillText("Andito lang ako palagi naka support sayo Iloveyouuu so much Bugsbunny ❤", canvas.width/2, (canvas.height/2 + 50));
         }
 
         secondOpacity = secondOpacity + 0.01;
@@ -211,10 +239,9 @@ function drawText() {
 
     if(frameNumber >= 3000 && frameNumber < 99999){
         context.fillStyle = `rgba(45, 45, 255, ${thirdOpacity})`;
-        context.fillText("Happy Valentine's Day <3", canvas.width/2, (canvas.height/2 + 120));
+        context.fillText("Happy Valentine's Day My Palagi ❤", canvas.width/2, (canvas.height/2 + 120));
         thirdOpacity = thirdOpacity + 0.01;
 
-        button.style.display = "block";
     }   
 
      // Reset the shadow effect after drawing the text
